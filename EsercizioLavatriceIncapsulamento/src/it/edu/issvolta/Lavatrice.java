98160fbbc1a9b2b675b0af1f068eb12c59c78de9
barbe <barbe@Mikko2>
2023-02-17 19:51:15 +0100

    Correzione esercizio

--- EsercizioLavatriceIncapsulamento/src/it/edu/issvolta/Lavatrice.java
+++ EsercizioLavatriceIncapsulamento/src/it/edu/issvolta/Lavatrice.java
@@ -8,7 +8,7 @@ public class Lavatrice {
 	Scanner r=new Scanner (System.in);
 	
 	private int capacità;
-	private int livelloAcqua;
+	private int livelloAcqua;  //max 6 litri
 	private int temperatura;
 	private int velocitàRotazione;
 	private int stato; 
@@ -17,6 +17,11 @@ public class Lavatrice {
 	private LocalTime dataOraAvvio;
 	
 	/*
+	lt=LocatTime.now();
+	System.out.println(SECONDS.between(dataOraAvvio, lt));  	//ogni volta che si chiama il getStato confronta il dataOraAvvio col secondo local time
+	
+	if(SECONDS.between(dataOraAvvio, lt)>60) //se sono passati più di60 secondi e lo stato attuale è 1 il lavaggio si ferma e lo stato diventa 2
+	 
 	* La lavatrice può partire se:
 	* 	-il credito è sufficiente (il costo lavaggio dipende dalla capacità
 	* 	(8kg=4,5 euro, 10kg=5,5 euro, 14kg=6,5 euro)
@@ -31,7 +36,15 @@ public class Lavatrice {
 	
 	public Lavatrice (int capacità)
 	{
-		this.capacità = capacità;
+		super();
+		if(capacità!=8 || capacità!=10 || capacità!=14)
+		{
+			System.out.println("Capacità non valida! Impostata capacità predefinita di 8 kg");
+			this.capacità=8;
+		}
+		else
+			this.capacità = capacità;
+		
 		livelloAcqua = 0;
 		temperatura = 0;
 		velocitàRotazione = 0;
@@ -42,142 +55,149 @@ public class Lavatrice {
 	
 	public void aggiungiAcqua(int litri) //livello max di acqua 6 litri
 	{
-		System.out.println("La quantità attuale di acqua nella lavatrice è " +livelloAcqua +" litri.");
-		System.out.println("Quanti litri vuoi aggiungere per il lavaggio? Livello massimo di acqua = 6 litri.");
-		do 
-		{
-			litri=r.nextInt();
+		if(litri<=0)
+			System.out.println("Quantità di acqua negativa, quindi non valida!");
+		else	
 			if(livelloAcqua+litri>6)
 				System.out.println("Il valore eccede il totale di acqua massima caricabile!");
-		} while (livelloAcqua+litri>6);
+			else
+				livelloAcqua+=litri;
+		
+		System.out.println("La quantità attuale di acqua nella lavatrice è " +livelloAcqua +" litri.");
+		System.out.println("Quanti litri vuoi aggiungere per il lavaggio? Livello massimo di acqua = 6 litri.");
 	}
 	
 	public void setTemperatura(int gradi) //la temperatura può essere 40, 60 90 gradi;
 	{
 		System.out.println("Quale ciclo di lavaggio vuoi usare? 40/60/90.");
-		do 
-		{
-			gradi=r.nextInt();
-			if(gradi!=40 || gradi!=60 || gradi !=90)
-				System.out.println("Temperatura errata! Scegliere 40/60/90.");
-		} while (gradi!=40 || gradi!=60 || gradi !=90);
+		
+		if(gradi==40 || gradi==60 || gradi ==90)
+			this.temperatura=gradi;
+		else 
+			System.out.println("Temperatura non valida!");
 	}
 	
 	public void setVelocitàRotazione(int velocitàRotazione) // la velocità può essere 600giri/m 800giri/m 1000giri/m
 	{
-		System.out.println("Imposta una velocità. 600/800/1000 giri/minuto.");
-		do 
-		{
-			velocitàRotazione=r.nextInt();
-			if(velocitàRotazione!=600 || velocitàRotazione!=800 || velocitàRotazione!=1000)
-				System.out.println("Velocità errata! Scegliere 600/800/1000.");
-		} while (velocitàRotazione!=600 || velocitàRotazione!=800 || velocitàRotazione!=1000);
-		
+		if(velocitàRotazione==600 || velocitàRotazione==800 || velocitàRotazione==1000)
+			this.velocitàRotazione=velocitàRotazione;
+		else
+			System.out.println("Velocità di rotazione non valida!");
 	}
 	
 	public void aggiungiDetersivo() //detersivo vero o falso
 	{
-		if(detersivo=false)
-			System.out.println("Detersivo non presente!");
-		else 
-		{
-			System.out.println("Premere enter per aggiungere detersivo");
-			r.nextLine();
-			detersivo=true;
-		}
+		detersivo=true;
 	}
 	
 	public void aggiungiMoneta(float moneta)
 	{
-		System.out.println("Inserisci la moneta...");
-		
-		do 
-		{
-			moneta=r.nextFloat();
-			if(moneta!=0.10f || moneta!=0.20f || moneta!=0.50f || moneta!=1.0f || moneta!=2.0f)
-				System.out.println("Moneta errata! Inserire solo monete da 0.10/0.20/0.50/1/2 Euro");
-		} while (moneta!=0.10f || moneta!=0.20f || moneta!=0.50f || moneta!=1.0f || moneta!=2.0f);
-	}
-	
-	public void start() //impostare il local time dataOraAvvio
-	{
-		System.out.println("La lavatrice è partita.");
-		dataOraAvvio=LocalTime.now();
-		stato=1;
-	}
-	
-	public void stop() //funziona con lo stato 1 e lo porta a 2
-	{
-		System.out.println("La lavatrice si è fermata");
-		stato=2;
-	}
-	
-	public void svuota() //riporta lo stato a 0 (se lo stato attuale è 2)
-	{
-		System.out.println("La lavatrice ora è vuota e pronta per un'altra macchinata.");
-		stato=0;
-		
-	}
-	public String getStato()
-	{
-		
-		switch (stato)		//stato: 0=libera 1=lavaggio in corso 2=lavaggio terminato
-		{
-		case 0:
-			System.out.println("Lavatrice libera."); //restituisce una stringa corrispondente allo stato
-			break;
-		case 1:
-			System.out.println("Lavaggio in corso.");
-			break;
-		case 2:
-			System.out.println("Lavaggio terminato");
-			break;
-			default:
-				break;
-		}
-		/*
-		lt=LocatTime.now();
-		System.out.println(SECONDS.between(dataOraAvvio, lt));  	//ogni volta che si chiama il getStato confronta il dataOraAvvio col secondo local time
-		
-		if(SECONDS.between(dataOraAvvio, lt)>60) //se sono passati più di60 secondi e lo stato attuale è 1 il lavaggio si ferma e lo stato diventa 2
-		 */
-	}
-	
-	public int getLivelloAcqua()
-	{
-		
-	}
-	
-	public int getTemperatura()
-	{
-		
-	}
-	
-	public int getVelocitàRotazione()
-	{
-		
-	}
-	
-	public int getCapacità()
-	{
-		
-	}
-	
-	public boolean detersvioAggiunto()
-	{
-		
-	}
-	
-	public float getCredito()
-	{
-		
+		if(moneta==0.10 || moneta==0.20 || moneta==0.50 || moneta==1.0 || moneta==2.0)
+			credito+=moneta;
+		else
+			System.out.println("Moneta non valida!");
 	}
 	
 	public float getCosto()
 	{
-		//restituisce i prezzi della lavatrice in euro(in base alla capacità)
+		switch(capacità) 
+		{
+		case 8:
+			return 4.50f;
+		case 10:
+			return 5.50f;
+		case 14:
+			return 6.50f;
+		default:
+			return 0;
+		}
+	}
+	
+	public void start() //impostare il local time dataOraAvvio
+	{
+		if(credito<getCosto())
+			System.out.println("Credito insufficiente!");
+		else if(livelloAcqua<4)
+			System.out.println("Livello acqua insufficiente!");
+		else if (temperatura==0)
+			System.out.println("Temperatura non impostata!");
+		else if(velocitàRotazione==0)
+			System.out.println("Velocità di rotazione non impostata!");
+		else if(!detersivo)
+			System.out.println("Detersivo non aggiunto!");
+		else if(stato!=0)
+			System.out.println("La lavatrice non è libera");
+		else 
+		{
+			stato=1;
+			dataOraAvvio=LocalTime.now();
+			System.out.println("La lavatrice è partita.");
+		}
+	}
+	
+	public void stop() //funziona con lo stato 1 e lo porta a 2
+	{
+		if(stato==1)
+		{
+			stato=2;
+			System.out.println("La lavatrice si è fermata");
+		}
+		else 
+			System.out.println("Lavatrice già ferma!");
+	}
+	
+	public String getStato()
+	{
+		switch(stato)
+		{
+		case 0:
+			return "libera";
+		case 1:
+			return "lavaggio in corso";
+		case 2:
+			return "lavaggio terminato";
+		default:
+			return "";
+		}
 	}
 
+	public int getCapacità() 
+	{
+		return capacità;
+	}
+
+	public int getLivelloAcqua() 
+	{
+		return livelloAcqua;
+	}
+
+	public int getTemperatura() 
+	{
+		return temperatura;
+	}
+
+	public int getVelocitàRotazione() 
+	{
+		return velocitàRotazione;
+	}
 	
+	public boolean detersivoAggiunto()
+	{
+		return detersivo;
+	}
 	
+	public float getCredito()
+	{
+		return credito;
+	}
+	
+	public void svuota() //riporta lo stato a 0 (se lo stato attuale è 2)
+	{
+		if(stato==2)
+			stato=0;
+		else if (stato==0)
+			System.out.println("Lavatrice già vuota");
+		else 
+			System.out.println("Lavaggio in corso");
+	}
 }
